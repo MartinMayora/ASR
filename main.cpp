4a17bc299f1a8617d2261c3d841fc689499aa2d4
MartinMayora <martinm@LAPTOP-27LB2UBB.localdomain>
2025-08-09 00:06:04 -0300

    removed debug comments

--- main.cpp
+++ main.cpp
@@ -6,20 +6,20 @@ int main(){
     int32_t samplingRate;
     std::vector<int16_t> wav;
     wav = readWav(test, samplingRate);
-    std::cout << samplingRate << "   "; 
-    std::cout<< wav.size() << "\n";
+
     std::vector<std::vector<double>> frames = frameSignal(wav);
     std::vector<std::vector<double>> powerSpect; //could be optimized, one vector with strides
     powerSpect.reserve(frames.size());
-    std::cout << frames.size();
+    
     for(auto& frame: frames){
         auto windowed = hammingWindowing(frame);
         auto dft = DFT(windowed);
         auto pSpecturm = powerSpectrum(dft);
         powerSpect.push_back(pSpecturm);
     }
-    int nMels = 30;
-    std::vector<std::vector<double>> filterBank = createMelFilterbank(samplingRate, powerSpect[0].size(), nMels);
+    
+    int nMels;
+    std::vector<std::vector<double>> filterBank = createMelFilterbank(samplingRate, powerSpect[0].size(), nMels = 30);
     std::vector<std::vector<double>> melEnergies = computeMelEnergies(powerSpect, filterBank);
     
     return 0;
